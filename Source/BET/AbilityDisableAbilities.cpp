99db256bc529bec9c599fbd3721c848abc3aed78
thebigglebomber <jason.worth@RM116-14.students.stl.yec>
2016-02-29 19:25:38 -0500

    effect over time
    made the class for effect over time and added a link to the
    disableability.cpp to help us determine who was hit by the ability.

--- Source/BET/AbilityDisableAbilities.cpp
+++ Source/BET/AbilityDisableAbilities.cpp
@@ -3,6 +3,7 @@
 #include "BET.h"
 #include "AbilityDisableAbilities.h"
 
-
+//https://docs.unrealengine.com/latest/INT/API/Runtime/Engine/Engine/UWorld/OverlapMultiByChannel/index.html
+//this link determines what pawns are affected by this and the area that it applies it to.
 
 
---
+++ Source/BET/EffectOverTime.h
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/Actor.h"
+#include "EffectOverTime.generated.h"
+
+UCLASS()
+class BET_API AEffectOverTime : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AEffectOverTime();
+
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	
+	// Called every frame
+	virtual void Tick( float DeltaSeconds ) override;
+	
+};

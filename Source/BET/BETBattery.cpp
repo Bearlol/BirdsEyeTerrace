f3f5e1b22de9ebab76e39729f14f869f0f831f78
brandon2499 <Brandon.Dunbar@students.stl.yec>
2016-06-01 18:31:38 -0400

    UI elements & general fixes
    Added some ui elements on keys, and locked doors.
    Fixed colliders on multiple objects and added a few sounds.

--- Source/BET/BETBattery.cpp
+++ Source/BET/BETBattery.cpp
@@ -12,6 +12,8 @@ ABETBattery::ABETBattery()
 	InteractAudio = NULL;
 	Character = nullptr;
 	OnActorBeginOverlap.AddDynamic(this, &ABETBattery::OnOverlap);
+	PrimaryActorTick.bCanEverTick = true;
+	WidgetActive = false;
 }
 
 
@@ -19,15 +21,37 @@ void ABETBattery::Interact()
 {
 	if (Character != nullptr)
 	{
-		Character->SetBattery();
-		UGameplayStatics::PlaySoundAtLocation(this, InteractAudio, GetActorLocation());
-		UE_LOG(LogTemp, Display, TEXT("Battery Restored"));
-		this->Destroy();
+		if (WidgetActive == false) 
+		{
+			Character->SetBattery();
+			UGameplayStatics::PlaySoundAtLocation(this, InteractAudio, GetActorLocation());
+			this->SetActorHiddenInGame(true);
+			this->SetActorEnableCollision(false);
+			UE_LOG(LogTemp, Display, TEXT("Battery Restored"));
+			WidgetInstance = CreateWidget<UUserWidget>(Cast<APlayerController>(Character->GetController()), UIWidget);
+			WidgetInstance->AddToViewport();
+			duration = 2.0f;
+			WidgetActive = true;
+		}
 	}
 }
 void ABETBattery::OnOverlap(AActor* OtherActor)
 {
 	Character = Cast<ABETCharacter>(OtherActor);
+}
+
+void ABETBattery::Tick(float DeltaTime)
+{
+	if (WidgetActive)
+	{
+		duration -= DeltaTime;
+		if (duration <= 0.f)
+		{
+			WidgetInstance->RemoveFromViewport();
+			WidgetInstance->Destruct();
+			this->K2_DestroyActor();
+		}
+	}
 }
 
 
